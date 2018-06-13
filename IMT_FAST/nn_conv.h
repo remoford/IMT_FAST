cbc35fa4adf34a527bb1c431967b26b8d4f75bad
John Ford <remoford@gmail.com>
2018-06-13 12:22:26 -0400

    Added initial version of nn_conv and binned_conv
    nn_conv standing for nearest neighbor convolution is a direct copy of hte existing approxconvolv_replacement
    
    binned_conv is a replacement for nn_conv that implements binned integration to more closely match our data

---
+++ IMT_FAST/nn_conv.h
@@ -0,0 +1,21 @@
+
+
+#ifndef APPROXCONVOLV_H
+#define APPROXCONVOLV_H
+
+/* Include files */
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "omp.h"
+
+/* Function Declarations */
+
+
+
+extern void nn_conv(const double z[], const double y[], const double X
+	[], const double x[], double Y[], double *logP0, int size_xyz, int size_XY, double h);
+
+#endif
+
