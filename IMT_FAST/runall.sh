20f466dae1d8c5473ab284d66c7c739fa1bcf22c
John Ford <remoford@gmail.com>
2018-07-02 00:43:34 -0500

    Modified runall.sh to work with SGE clusters. Works great!
    Also included copies of the libgsl shared libraries as they weren't installed on mrgreen

--- IMT_FAST/runall.sh
+++ IMT_FAST/runall.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
-
-datestamp=$(date +%s)
-
-mkdir "output/""$datestamp"
-pushd "output/""$datestamp"
-
-pwd
-
-for mode in onestage twostage threestage
-do
-	for data in  ../../data/*
-	do
-		../../icc_fast $mode $data > "icc_""$mode""_""$(basename $data | sed 's/\.txt$//')""_out.txt" &
-		../../gcc_fast $mode $data > "gcc_""$mode""_""$(basename $data | sed 's/\.txt$//')""_out.txt" &
-	done
-done
-
-
-wait
-popd
+#$ -t 1-18
+#$ -e /dev/null
+#$ -o /dev/null
+task_index=$(( $SGE_TASK_ID - 1  ))
+data_sets=(AT1  CHX  DMSO  erlotinib  FUCCI  MCF)
+modes=(onestage twostage threestage)
+datestamp="_""$(date +%s)"
+if [$JOB_NAME -eq ""]
+then
+	out_dir="output/""$datestamp"
+	mkdir $out_dir
+	rm output/latest
+	ln -s $(pwd)/$out_dir $(pwd)/output/latest
+	qsub -N $datestamp -cwd $0 -e /dev/null -o /dev/null
+	sleep 10
+	qstat
+	find $out_dir | grep out.txt | xargs -n1 head -n1
+else
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+	data=${data_sets[ $(( $task_index % 6 )) ]}
+	mode=${modes[ $(( $task_index / 6 )) ]}
+	binary="./gcc_fast"
+	output_dir="output/""$JOB_NAME""/"
+	(
+		echo -n "Job ""$JOB_ID"".""$task_index"" started on ""$HOSTNAME"" from ""$SGE_O_HOST"" logging to ""$output_dir"
+		echo
+		date
+		echo
+		uptime
+		echo
+		sed 's/^$/%/' < /proc/cpuinfo | tr '\n' '^' | tr '%' '\n' | head -n1 | tr '^' '\n'
+		echo $(grep "^processor" /proc/cpuinfo | wc -l)" threads"
+		echo
+		free -h
+		echo
+		df -h
+		echo
+		echo "Running $binary $mode $data ..."
+		echo
+		time ($binary $mode "data/""$data"".txt" | grep -v "range error")
+	) 2>&1 > "$output_dir""$binary""_""$mode""_""$data""_out.txt"
+fi
